d9bbcfe51a4dcfb385b31e823443488c77d6b980
haights <76668500+haights@users.noreply.github.com>
2024-07-16 09:07:54 -0700

    Added a small debug statement

--- app.py
+++ app.py
@@ -125,8 +125,8 @@ st.title('Image Classification with PyTorch and Streamlit')
 uploaded_file = st.file_uploader("Choose an image...", type="jpg")
 
 if uploaded_file is not None:
-    image = Image.open(uploaded_file)
     st.write("uploaded file is not None")
+    image = Image.open(uploaded_file)
     st.image(image, caption='Uploaded Image.', use_column_width=True)
     st.write("")
     st.write("Classifying...")
