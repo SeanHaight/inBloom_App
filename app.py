715130f512721835557d958087f92a3c0921eca9
haights <76668500+haights@users.noreply.github.com>
2024-07-16 08:53:06 -0700

    Added a small debug statement

--- app.py
+++ app.py
@@ -126,6 +126,7 @@ uploaded_file = st.file_uploader("Choose an image...", type="jpg")
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
+    st.write("uploaded file is not None")
     st.image(image, caption='Uploaded Image.', use_column_width=True)
     st.write("")
     st.write("Classifying...")
